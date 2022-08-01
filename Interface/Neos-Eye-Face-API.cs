dbc1db5236afaf56e33ea8aa59ed3f34f40502ec
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-08-01 11:41:12 -0500

    big bang

--- Interface/Neos-Eye-Face-API.cs
+++ Interface/Neos-Eye-Face-API.cs
@@ -1,22 +1,27 @@
-﻿using HarmonyLib;
-using NeosModLoader;
+﻿using BaseX;
 using FrooxEngine;
-using BaseX;
+using HarmonyLib;
+using NeosModLoader;
 using System;
 
-namespace Neos_EyeFace_API
+namespace ProjectBabbleNeos
 {
-	public class Neos_EyeFace_API : NeosMod
+    public class ProjectBabbleNeos : NeosMod
 	{
-		public override string Name => "Neos_EyeFace_API";
+		public override string Name => "ProjectBabble-Neos";
 		public override string Author => "dfgHiatus";
-		public override string Version => "1.1.1";
+		public override string Version => "1.0.0";
 		public override string Link => "https://github.com/dfgHiatus/Neos-Eye-Face-API/";
+
+		private static BabbleOSC _bosc;
+		private static ModConfiguration _config;
+
 		public override void OnEngineInit()
 		{
 			// Harmony.DEBUG = true;
-			new Harmony("net.dfgHiatus.Neos-EyeFace-API").PatchAll();
-			Engine.Current.OnShutdown += () => Shutdown();
+			new Harmony("net.dfgHiatus.ProjectBabble-Neos").PatchAll();
+			_config = GetConfiguration();
+			Engine.Current.OnShutdown += () => _bosc.Teardown();
         }
 
 		[HarmonyPatch(typeof(InputInterface), MethodType.Constructor)]
@@ -27,7 +32,8 @@ namespace Neos_EyeFace_API
 			{
 				try
 				{
-					GenericInputDevice gen = new GenericInputDevice();
+					BabbleOSC ETVR = new BabbleOSC();
+					ProjectBabbleDevice gen = new ProjectBabbleDevice();
                     __instance.RegisterInputDriver(gen);
 				}
 				catch (Exception e)
@@ -37,111 +43,63 @@ namespace Neos_EyeFace_API
 				}
 			}
 		}
-		private void Shutdown()
+
+		public class ProjectBabbleDevice : IInputDriver
 		{
-			// Shutdown code goes here
-			// ...
-			// ...
-		}
-		public class GenericInputDevice : IInputDriver
-		{
-			private Eyes _eyes;
 			private Mouth _mouth;
 			public int UpdateOrder => 100;
 
 			public void CollectDeviceInfos(DataTreeList list)
 			{
-				DataTreeDictionary eyeDataTreeDictionary = new DataTreeDictionary();
-				eyeDataTreeDictionary.Add("Name", "Generic Eye Tracking");
-				eyeDataTreeDictionary.Add("Type", "Eye Tracking");
-				eyeDataTreeDictionary.Add("Model", "Generic Eye Model");
-				list.Add(eyeDataTreeDictionary);
-
 				DataTreeDictionary mouthDataTreeDictionary = new DataTreeDictionary();
-				mouthDataTreeDictionary.Add("Name", "Generic Face Tracking");
+				mouthDataTreeDictionary.Add("Name", "Project Babble Face Tracking");
 				mouthDataTreeDictionary.Add("Type", "Face Tracking");
-				mouthDataTreeDictionary.Add("Model", "Generic Face Model");
+				mouthDataTreeDictionary.Add("Model", "Project Babble Model");
 				list.Add(mouthDataTreeDictionary);
 			}
 
 			public void RegisterInputs(InputInterface inputInterface)
 			{
-				_eyes = new Eyes(inputInterface, "Generic Eye Tracking");
-				_mouth = new Mouth(inputInterface, "Generic Mouth Tracking");
+				_mouth = new Mouth(inputInterface, "Project Babble Mouth Tracking");
 			}
 
 			public void UpdateInputs(float deltaTime)
 			{
-				UpdateEyes(deltaTime);
-				UpdateMouth(deltaTime);
-			}
-
-			private void UpdateEyes(float deltaTime)
-			{
-				_eyes.IsEyeTrackingActive = _eyes.IsEyeTrackingActive;
-
-				UpdateEye(float3.Zero, float3.Zero, true, 0.003f,
-					1f, 0f, 0f, 0f, deltaTime, _eyes.LeftEye);
-				UpdateEye(float3.Zero, float3.Zero, true, 0.003f,
-					1f, 0f, 0f, 0f, deltaTime, _eyes.RightEye);
-
-				UpdateEye(float3.Zero, float3.Zero, true, 0.003f,
-					1f, 0f, 0f, 0f, deltaTime, _eyes.CombinedEye);
-				_eyes.ComputeCombinedEyeParameters();
-
-				_eyes.ConvergenceDistance = 0f;
-				_eyes.Timestamp += deltaTime;
-				_eyes.FinishUpdate();
-			}
-			private void UpdateEye(float3 gazeDirection, float3 gazeOrigin, bool status, float pupilSize, float openness,
-				float widen, float squeeze, float frown, float deltaTime, Eye eye)
-			{
-				eye.IsDeviceActive = Engine.Current.InputInterface.VR_Active;
-				eye.IsTracking = status;
-
-				if (eye.IsTracking)
-				{
-					eye.UpdateWithDirection(gazeDirection);
-					eye.RawPosition = gazeOrigin;
-					eye.PupilDiameter = pupilSize;
-				}
-
-				eye.Openness = openness;
-				eye.Widen = widen;
-				eye.Squeeze = squeeze;
-				eye.Frown = frown;
-			}
-
-			private void UpdateMouth(float deltaTime)
-			{
 				_mouth.IsDeviceActive = true;
-
 				_mouth.IsTracking = true;
 
-				_mouth.Jaw = float3.Zero;
-				_mouth.Tongue = float3.Zero;
+				// Assuming x is left/right, y is up/down, z is forward/backwards
+				_mouth.Jaw = new float3(
+					BabbleOSC.MouthShapes["jawLeft"] - BabbleOSC.MouthShapes["jawRight"],
+					BabbleOSC.MouthShapes["jawOpen"] + BabbleOSC.MouthShapes["mouthClose"] * -1,
+					BabbleOSC.MouthShapes["jawForward"]);
+				_mouth.Tongue = new float3(
+					0f,
+					0f,
+					BabbleOSC.MouthShapes["tongueOut"]);
 
-				_mouth.JawOpen = 0f;
-				_mouth.MouthPout = 0f;
+				_mouth.JawOpen = BabbleOSC.MouthShapes["jawOpen"];
+				_mouth.MouthPout = BabbleOSC.MouthShapes["mouthPucker"] - BabbleOSC.MouthShapes["mouthFunnel"];
 				_mouth.TongueRoll = 0f;
 
-				_mouth.LipBottomOverUnder = 0f;
+				_mouth.LipBottomOverUnder = BabbleOSC.MouthShapes["mouthRollLower"] * -1;
 				_mouth.LipBottomOverturn = 0f;
-				_mouth.LipTopOverUnder = 0f;
+				_mouth.LipTopOverUnder = BabbleOSC.MouthShapes["mouthRollUpper"] * -1;
 				_mouth.LipTopOverturn = 0f;
 
-				_mouth.LipLowerHorizontal = 0f;
-				_mouth.LipUpperHorizontal = 0f;
+				// Assuming a tug face like this? => 0_0
+				_mouth.LipLowerHorizontal = BabbleOSC.MouthShapes["mouthDimple_L"] - BabbleOSC.MouthShapes["mouthDimple_R"];
+				_mouth.LipUpperHorizontal = BabbleOSC.MouthShapes["mouthDimple_L"] - BabbleOSC.MouthShapes["mouthDimple_R"];
 
-				_mouth.LipLowerLeftRaise = 0f;
-				_mouth.LipLowerRightRaise = 0f;
-				_mouth.LipUpperRightRaise = 0f;
-				_mouth.LipUpperLeftRaise = 0f;
+				_mouth.LipLowerLeftRaise = BabbleOSC.MouthShapes["mouthLowerDown_L"];
+				_mouth.LipLowerRightRaise = BabbleOSC.MouthShapes["mouthLowerDown_R"];
+				_mouth.LipUpperRightRaise = BabbleOSC.MouthShapes["mouthUpperUp_R"];
+				_mouth.LipUpperLeftRaise = BabbleOSC.MouthShapes["mouthUpperUp_L"];
 
-				_mouth.MouthRightSmileFrown = 0f;
-				_mouth.MouthLeftSmileFrown = 0f;
-				_mouth.CheekLeftPuffSuck = 0f;
-				_mouth.CheekRightPuffSuck = 0f;
+				_mouth.MouthRightSmileFrown = BabbleOSC.MouthShapes["mouthSmile_L"] - BabbleOSC.MouthShapes["mouthFrown_L"];
+				_mouth.MouthLeftSmileFrown = BabbleOSC.MouthShapes["mouthSmile_R"] - BabbleOSC.MouthShapes["mouthFrown_R"];
+				_mouth.CheekLeftPuffSuck = BabbleOSC.MouthShapes["cheekPuff"];
+				_mouth.CheekRightPuffSuck = BabbleOSC.MouthShapes["cheekPuff"];
 			}
 		}
 	}
---
+++ Interface/OSCClient.cs
@@ -0,0 +1,124 @@
+﻿using BaseX;
+using Rug.Osc;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace ProjectBabbleNeos
+{
+    public class BabbleOSC
+    {
+        public static Dictionary<string, float> MouthShapes = new Dictionary<string, float>();
+        private static OscReceiver _receiver;
+        private static Thread _thread;
+        private const int DEFAULT_PORT = 9000;
+
+        public BabbleOSC()
+        {
+            if (_receiver != null)
+            {
+                UniLog.Error("BabbleOSC connection already exists.");
+                return;
+            }
+
+            _receiver = new OscReceiver(DEFAULT_PORT);
+            ConstructorHelper();
+        }
+
+        public BabbleOSC(int port)
+        {
+            if (_receiver != null)
+            {
+                UniLog.Error("BabbleOSC connection already exists.");
+                return;
+            }
+
+            _receiver = new OscReceiver(port);
+            ConstructorHelper();
+        }
+
+        private void ConstructorHelper()
+        {
+            // I want to use an enum, but we need to deal with strings
+            foreach (var shape in MouthShape)
+            {
+                MouthShapes.Add(shape, 0f);
+            }
+            _thread = new Thread(new ThreadStart(ListenLoop));
+            _receiver.Connect();
+            _thread.Start();
+        }
+
+        private static void ListenLoop()
+        {
+            OscPacket packet;
+            OscMessage message;
+            float candidate = 0;
+
+            while (_receiver.State != OscSocketState.Closed) {
+                try {
+                    if (_receiver.State == OscSocketState.Connected) {
+                        packet = _receiver.Receive();
+                        if (OscMessage.TryParse(packet.ToString(), out message)) {
+                            if (MouthShapes.ContainsKey(message.Address)) {
+                                if (float.TryParse(message[0].ToString(), out candidate)) {
+                                    MouthShapes[message.Address] = candidate;
+                                }
+                            }
+                        }
+                    }
+                    Thread.Sleep(10);
+                }
+                catch (Exception e)
+                {
+                    if (_receiver.State == OscSocketState.Connected)
+                        UniLog.Error(e.Message);
+                }
+            }
+        }
+
+        public void Teardown()
+        {
+            _receiver.Close();
+            _thread.Join();
+        }
+
+        // Ideally this should be in its own class
+        private static string[] MouthShape = new string[]
+        {
+            "cheekPuff",
+            "cheekSquint_L",
+            "cheekSquint_R",
+            "noseSneer_L",
+            "noseSneer_R",
+            "jawOpen",
+            "jawForward",
+            "jawLeft",
+            "jawRight",
+            "mouthFunnel",
+            "mouthPucker",
+            "mouthLeft",
+            "mouthRight",
+            "mouthRollUpper",
+            "mouthRollLower",
+            "mouthShrugUpper",
+            "mouthShrugLower",
+            "mouthClose",
+            "mouthSmile_L",
+            "mouthSmile_R",
+            "mouthFrown_L",
+            "mouthFrown_R",
+            "mouthDimple_L",
+            "mouthDimple_R",
+            "mouthUpperUp_L",
+            "mouthUpperUp_R",
+            "mouthLowerDown_L",
+            "mouthLowerDown_R",
+            "mouthPress_L",
+            "mouthPress_R",
+            "mouthStretch_L",
+            "mouthStretch_R",
+            "tongueOut",
+        };
+    }
+}
