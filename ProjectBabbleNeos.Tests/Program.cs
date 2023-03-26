9f8d799f8bd228076c2e9d6afad5cd036623ad76
plyshka <leruop@gmail.com>
2023-03-27 01:56:35 +0500

    Ported to .net

--- ProjectBabbleNeos.Tests/Program.cs
+++ ProjectBabbleNeos.Tests/Program.cs
@@ -1,3 +1,15 @@
-﻿using ProjectBabbleNeos;
+﻿using System;
 
-new BabbleOSC();
+namespace ProjectBabbleNeos.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BabbleOSC client = new BabbleOSC();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+            client.Teardown();
+        }
+    }
+}
